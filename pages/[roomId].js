ff0fec5e44542f417541b7e9b6fa243f45672005
Ganesham Pareek <104836088+Ganesham09@users.noreply.github.com>
2024-03-15 00:02:20 +0530

    setup media stream and Lobby

--- pages/[roomId].js
+++ pages/[roomId].js
@@ -2,11 +2,19 @@ import { useEffect } from "react";
 
 import { useScoket } from "@/context/socket";
 import usePeer from "@/hooks/usePeer";
-import {} from "react";
+import useMediaStream from "@/hooks/useMediaStream";
+import Player from "@/components/player";
 
 const Room = () => {
   const socket = useScoket();
   const { peer, myId } = usePeer();
+  const { stream } = useMediaStream();
+
+  return (
+    <div>
+      <Player url={stream} muted playing playerId={myId} />
+    </div>
+  );
 };
 
 export default Room;
